016b70a118e379797d28489f4e3842bb91171240
Rahim00001 <rahimlpt2022@gmail.com>
2023-09-14 10:13:53 +0600

    unnecessary code removedand fake data added

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div>
+            <h1 className="text-5xl text-yellow-300 text-center font-bold m-5">Knowledge Cafe</h1>
+        </div>
+    );
+};
+
+export default Header;
